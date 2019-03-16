9f6a11d895112594ad7ca8366cf1f2728d67c28b
yagisuke <yagisuke.0209@gmail.com>
2019-03-17 00:20:11 +0900

    07

--- basic07-clean-urls-ssr/server.js
+++ basic07-clean-urls-ssr/server.js
@@ -9,9 +9,16 @@ app.prepare()
 .then(() => {
   const server = express()
 
+  // server.get('/p/:id', (req, res) => {
+  //   const actualPage = '/post'
+  //   const queryParams = { title: req.params.id } 
+  //   app.render(req, res, actualPage, queryParams)
+  // })
+
   server.get('/p/:id', (req, res) => {
     const actualPage = '/post'
-    const queryParams = { title: req.params.id } 
+    const queryParams = { id: req.params.id }
+    console.log(queryParams.id)
     app.render(req, res, actualPage, queryParams)
   })
 
