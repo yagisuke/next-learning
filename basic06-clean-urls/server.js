42a5cf7b6b381a46038e4b1f60a4f6d19f9c1051
yagisuke <yagisuke.0209@gmail.com>
2019-03-16 22:52:51 +0900

    06

--- basic06-clean-urls/server.js
+++ basic06-clean-urls/server.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const next = require('next')
-
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare()
-.then(() => {
+app.prepare().then(() => {
   const server = express()
 
   server.get('/p/:id', (req, res) => {
