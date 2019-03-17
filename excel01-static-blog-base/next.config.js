949729b33585ba5c1cdfc2e9461bfbacdf538630
yagisuke <yagisuke.0209@gmail.com>
2019-03-17 20:15:11 +0900

    ex01

--- excel01-static-blog-base/next.config.js
+++ excel01-static-blog-base/next.config.js
@@ -5,7 +5,8 @@ module.exports = {
       '/about': { page: '/about' },
       '/p/hello-nextjs': { page: '/post', query: { title: 'Hello Next.js' } },
       '/p/learn-nextjs': { page: '/post', query: { title: 'Learn Next.js is awesome' } },
-      '/p/deploy-nextjs': { page: '/post', query: { title: 'Deploy apps with Zeit' } }
+      '/p/deploy-nextjs': { page: '/post', query: { title: 'Deploy apps with Zeit' } },
+      '/p/exporting-pages': { page: '/post', query: { title: 'Learn to Export HTML Pages' } }
     }
   }
 }
